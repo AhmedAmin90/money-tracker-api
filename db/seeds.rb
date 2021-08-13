2464b356e873497ce41390daa4df3e41fb75c711
Ahmed Amin <qa.ahmed.amin10@gmail.com>
2021-08-13 22:05:32 +0300

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user_one = User.create(username: 'Ahmed' , email: 'ahmed@email.com' , password: 'testtest')
+user_two = User.create(username: 'Abdo' , email: 'abdo@email.com' , password: 'testtest')
