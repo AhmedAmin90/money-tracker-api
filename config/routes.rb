0c734da398261cd357500966de286d7ab5d9b270
Ahmed Amin <qa.ahmed.amin10@gmail.com>
2021-08-13 22:50:17 +0300

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,10 @@ Rails.application.routes.draw do
       
     end
   end
-  resources :users 
+  resources :users , only: [:index , :show]
+  devise_for :users, :controllers => {:registrations => "users/registrations"}
   resources :sessions
   # devise_for :users
-  devise_for :users, :controllers => {:registrations => "users/registrations"}
   root "users#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
