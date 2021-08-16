ba3cb750fd7f1530a90c13d75deaa71f14d8c2d4
Ahmed Amin <qa.ahmed.amin10@gmail.com>
2021-08-17 01:04:30 +0300

    Fix presence

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_many :expenses
-  validates :name , presence :true
+
+  validates :name, presence: true
   validates :user_id, presence: true, uniqueness: { scope: :name }
 
 end
