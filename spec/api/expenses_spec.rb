8bfbeb8f9a8458ab13477c4075fbe1a14b082b47
Ahmed Amin <qa.ahmed.amin10@gmail.com>
2021-08-19 12:25:18 +0300

    Add testing for api requests for my app

---
+++ spec/api/expenses_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+describe "Expenses api", type: :request do
+    describe "Get method" do
+        it "Returns all expenses" do
+            FactoryBot.create(:item, id: 1 , name: "Sport" , user_id: 1)
+            FactoryBot.create(:expense, expense: 50 , item_id: 1)
+            get '/api/v1/expenses' 
+            expect(response).to  have_http_status(:success)
+            expect(JSON.parse(response.body).size).to eq(1) 
+        end
+    end
+    
+    describe "Get method" do
+        it "Returns all expenses after adding another expense" do
+            FactoryBot.create(:item, id: 1 , name: "Sport" , user_id: 1)
+            FactoryBot.create(:expense, expense: 50 , item_id: 1)
+            FactoryBot.create(:expense, expense: 30 , item_id: 1)
+
+            get '/api/v1/expenses' 
+            expect(response).to  have_http_status(:success)
+            expect(JSON.parse(response.body).size).to eq(2) 
+        end
+    end
+
+    describe "Post method" do
+        it "Create a new expense" do
+            FactoryBot.create(:item, id:1 ,  name: "Shoping" , user_id: 1)
+            expect {
+                post '/api/v1/expenses' , params: {expense: 30 , item_id: 1}
+
+            }.to change {Expense.count }.from(0).to(1)
+            expect(response).to  have_http_status(:created)
+        end   
+    end
+    
+
+    describe "Delete method" do
+        it "Delete expense" do
+            FactoryBot.create(:item, id:1 , name: "cafe" , user_id: 1)
+            FactoryBot.create(:expense, id:1 , expense: 50 , item_id: 1)
+            delete '/api/v1/expenses/1'
+            expect(response).to  have_http_status(:no_content)
+        end  
+    end
+
+    
+end
---
+++ spec/factories/user.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+    factory :user do
+        username {'Ahmed'}
+        password {'123456'}
+    end
+end
